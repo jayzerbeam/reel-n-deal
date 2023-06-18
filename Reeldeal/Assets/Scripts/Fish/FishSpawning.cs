6eeafb0004e640d94d0a0026a1854be6aaba4463
GradySullivan <grady.sullivan18@gmail.com>
2023-06-18 19:52:44 -0400

    fishAI, commit 3
    weighted fish prefab spawning, fish prefab variants, additional comment/code cleanup

--- Reeldeal/Assets/Scripts/Fish/FishSpawning.cs
+++ Reeldeal/Assets/Scripts/Fish/FishSpawning.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class FishSpawning : MonoBehaviour
 {
+    [System.Serializable]
+    public class FishPrefabInfo
+    {
+        public GameObject prefabVariant;
+        public float weight;
+    }
+
+    struct PotentialFishPrefabs
+    {
+        public GameObject prefab;
+        public float weight;
+    }
+
     // game object intialization
-    public GameObject[] fishPrefabs;
+    public List<FishPrefabInfo> fishPrefabs;
     public LayerMask waterLayer;
 
     // spawn parameters
@@ -25,10 +38,10 @@ public class FishSpawning : MonoBehaviour
         spawnTimer = 5f;
 
         //add tags to fish prefabs
-        foreach (GameObject fishPrefab in fishPrefabs)
+        foreach (FishPrefabInfo fishPrefabInfo in fishPrefabs)
         {
-            FishPrefabInitializer prefabInitializer = fishPrefab.GetComponent<FishPrefabInitializer>(); // get the initializer script component of the prefab
-            prefabInitializer.Awake();
+            GameObject fishPrefab = fishPrefabInfo.prefabVariant;
+            fishPrefab.GetComponent<FishPrefabInitializer>().Awake();
         }
     }
 
@@ -72,48 +85,32 @@ public class FishSpawning : MonoBehaviour
 
         string waterTag = randomWaterCollider.gameObject.tag; // tag of chosen body of water
         Vector3 spawnPosition = GetRandomPointOnCollider(randomWaterCollider); // random spot on water that meets spawning criteria
-
+        //Debug.Log("waterTag: " + waterTag);
         if (spawnPosition == Vector3.zero)
         {
+            Debug.Log("Spawn Location Failure");
             return; // if no spot found
         }
 
         // pick prefab based on tags
-        GameObject fishPrefab;
-        List<GameObject> potentialFishPrefabs = new List<GameObject>();
-        foreach (GameObject prefab in fishPrefabs)
-        {
-            FishMultiTag prefabMultiTag = prefab.GetComponent<FishMultiTag>(); // Use the correct variable name here
-            if (prefabMultiTag.HasTag(waterTag))
-            {
-                potentialFishPrefabs.Add(prefab);
-            }
-        }
-        if (potentialFishPrefabs.Count > 0)
-        {
-            fishPrefab = potentialFishPrefabs[Random.Range(0, potentialFishPrefabs.Count)]; // randomly pick from available prefabs    
-        }
-        else
-        {
-            Debug.Log("No prefab found");
-            return; // no spawning if not found
-        }
+        GameObject fishPrefab = GetRandomFishPrefab(waterTag);
 
         // create fish (docs.unity3d.com/ScriptReference/Object.Instantiate.html)
         GameObject fish = Instantiate(fishPrefab, spawnPosition, Quaternion.identity);
 
         // all for debugging
         FishMultiTag fishMultiTag = fish.GetComponent<FishMultiTag>();
+        Debug.Log("------------");
         Debug.Log("Fish Spawned at " + spawnPosition);
         Debug.Log("Distance to player: " + Vector3.Distance(spawnPosition, playerTransform.position));
         Debug.Log("Fish Tags: " + string.Join(", ", fishMultiTag.tags));
+        Debug.Log("Water Type: " + waterTag);
     }
 
     private Vector3 GetRandomPointOnCollider(Collider collider)
     {
         
         Bounds bounds = collider.bounds; // get bounds of water plane (https://forum.unity.com/threads/what-are-bounds.480975/)
-
         // Calculate the ranges within the collider bounds
         float maxRange = Mathf.Min(maxSpawnDistance, Mathf.Max(bounds.size.x, bounds.size.y, bounds.size.z) / 2f);
         float minRange = Mathf.Min(minSpawnDistance, maxRange);
@@ -135,7 +132,7 @@ public class FishSpawning : MonoBehaviour
         }
 
         // return if no valid point found after a few attempts to prevent game freezes
-        Debug.Log("Failed to find a valid spawn point");
+        //Debug.Log("Failed to find a valid spawn point");
         return Vector3.zero;
     }
 
@@ -160,5 +157,46 @@ public class FishSpawning : MonoBehaviour
         }
         return false;
     }
+
+    
+
+    private GameObject GetRandomFishPrefab(string waterTag)
+    {
+        List<PotentialFishPrefabs> potentialFishPrefabs = new List<PotentialFishPrefabs>();
+        float totalWeight = 0f;
+
+        // determine potential prefabs
+        foreach (FishPrefabInfo variant in fishPrefabs)
+        {
+            FishMultiTag multiTag = variant.prefabVariant.GetComponent<FishMultiTag>();
+
+            if (multiTag != null && multiTag.HasTag(waterTag))
+            {
+                potentialFishPrefabs.Add(new PotentialFishPrefabs { prefab = variant.prefabVariant, weight = variant.weight });
+                //Debug.Log("Fish Tags: " + string.Join(", ", multiTag.tags) + " WEIGHT: " + variant.weight);
+                totalWeight += variant.weight;
+            }
+        }
+
+        // choose from potential prefab variants
+        if (potentialFishPrefabs.Count > 0)
+        {
+            float threshold = Random.Range(0f, totalWeight);
+            float cumulativeWeight = 0f;
+
+            foreach (PotentialFishPrefabs fishPrefabWeight in potentialFishPrefabs)
+            {
+                cumulativeWeight += fishPrefabWeight.weight;
+
+                if (threshold <= cumulativeWeight)
+                {
+                    return fishPrefabWeight.prefab;
+                }
+            }
+        }
+        return null;
+    }
+
+
 
 }

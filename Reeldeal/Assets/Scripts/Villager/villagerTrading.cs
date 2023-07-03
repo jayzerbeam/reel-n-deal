7e3ea233190362b09eb06381f8460a8a97868e4e
rootzull <laip3@gatech.edu>
2023-07-02 21:58:16 -0400

    remnove build error

--- Reeldeal/Assets/Scripts/Villager/villagerTrading.cs
+++ Reeldeal/Assets/Scripts/Villager/villagerTrading.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Content;
 using UnityEngine;
+//using UnityEditor.Build.Content;
 
 public class villagerTrading : MonoBehaviour
 {

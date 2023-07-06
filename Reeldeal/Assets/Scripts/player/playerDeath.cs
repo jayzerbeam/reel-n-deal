f50b93ab4fc68e291765b7714bfc609b32a8cb81
Cassandra Durkee <cdurkee6@gatech.edu>
2023-07-05 20:31:06 -0500

    update player death script so player dies after 3 seconds

--- Reeldeal/Assets/Scripts/player/playerDeath.cs
+++ Reeldeal/Assets/Scripts/player/playerDeath.cs
@@ -1,14 +1,46 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class playerDeath : MonoBehaviour
+
 {
-    private void OnTriggerEnter(Collider other)
+    private float waterTime;
+    private bool isInWater = false;
+
+    private void Update()
     {
-        if (other.CompareTag("Lake") || other.CompareTag("Waterfall") || other.CompareTag("River") || other.CompareTag("Spring") || other.CompareTag("Ocean"))
+
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene("MainMenu");
+            if (isInWater)
+            {
+                waterTime += Time.deltaTime;
+                if (waterTime >= 3f)
+                {
+                    SceneManager.LoadScene("MainMenu");
+                }
+
+
+            }
         }
     }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Lake") || c.CompareTag("Waterfall") || c.CompareTag("River") || c.CompareTag("Spring") || c.CompareTag("Ocean"))
+        {
+            isInWater = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.CompareTag("Lake") || c.CompareTag("Waterfall") || c.CompareTag("River") || c.CompareTag("Spring") || c.CompareTag("Ocean"))
+        {
+            isInWater = false;
+            waterTime = 0f;
+        }
+
+    }
+
+
 }

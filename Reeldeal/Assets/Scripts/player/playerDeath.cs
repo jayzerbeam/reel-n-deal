60d897e11cf32bf9b842b8177717f70d43b306d4
Cassandra Durkee <cdurkee6@gatech.edu>
2023-07-22 12:32:34 -0500

    change death transition, update PlayerDrownVolume.cs and playerDeath.cs

--- Reeldeal/Assets/Scripts/player/playerDeath.cs
+++ Reeldeal/Assets/Scripts/player/playerDeath.cs
@@ -5,69 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class playerDeath : MonoBehaviour
 {
-    private float waterTime;
-    private bool isInWater = false;
-    private Animator animator;
+    public PlayerDrownVolume playerDrownVolume;
+    public Animator dyingAnimation; 
 
     public bool isDying;
 
-    private void Awake()
+    private void Start()
     {
-        animator = GetComponent<Animator>();
+
     }
 
-    private void Update()
+    private void Drown()
     {
-        if (isInWater)
-        {
-            waterTime += Time.deltaTime;
-            if (waterTime >= 3f)
-            {
-                animator.SetTrigger("isDying");
-                Invoke("LoadMainMenu", 8f);
-            }
-        }
+       // transform.position = respawnPosition;
+
+        playerDrownVolume.isDrowning = true; 
     }
 
-    private void OnTriggerEnter(Collider c)
-    {
-        if (
-            c.CompareTag("Lake")
-            || c.CompareTag("Waterfall")
-            || c.CompareTag("River")
-            || c.CompareTag("Spring")
-            || c.CompareTag("Ocean")
-        )
-        {
-            isInWater = true;
-        }
-    }
 
-    private void OnTriggerExit(Collider c)
-    {
-        if (
-            c.CompareTag("Lake")
-            || c.CompareTag("Waterfall")
-            || c.CompareTag("River")
-            || c.CompareTag("Spring")
-            || c.CompareTag("Ocean")
-        )
-        {
-            isInWater = false;
-            waterTime = 0f;
-        }
-    }
 
     private void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
 
-    private void isDead()
+    private void Die()
     {
         if (isDying)
         {
-            animator.SetTrigger("isDying");
+            dyingAnimation.SetBool("isDrowning", true);
         }
     }
 }

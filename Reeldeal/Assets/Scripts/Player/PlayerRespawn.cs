17ce9e535a1fe2124085c4964c690bfcb6e8298d
Jason Long <jal@gatech.edu>
2023-07-25 17:14:18 -0500

    death and respawn anims

--- Reeldeal/Assets/Scripts/Player/PlayerRespawn.cs
+++ Reeldeal/Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,18 +1,23 @@
- using System;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro; 
+using TMPro;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    public Transform respawnPoint; 
+    public Transform respawnPoint;
     private float respawnYThreshold = 47f;
+    private Animator _animator;
+    private Rigidbody _rb;
 
     private CharacterController characterController;
     private Vector3 initialPosition;
+    private float dyingAnimCountdown;
     private float timeInWater;
     private float timeThreshold = 5f;
-    private bool isDyingTriggered = false;
+    private int _isDeadHash;
+    private bool _isDeadAnim;
+    private bool _isDead = false;
     public GameObject waterAlert;
     private GameObject waterCountdown;
     private TextMeshProUGUI countdownText;
@@ -23,9 +28,12 @@ public class PlayerRespawn : MonoBehaviour
 
     private void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
         initialPosition = transform.position;
         coinInventory = FindObjectOfType<hud_gui_controller>();
+        _animator = GetComponent<Animator>();
+        _isDeadHash = Animator.StringToHash("isDead");
 
         waterCountdown = waterAlert.transform.Find("Countdown").gameObject;
         countdownText = waterCountdown.GetComponent<TextMeshProUGUI>();
@@ -33,60 +41,73 @@ public class PlayerRespawn : MonoBehaviour
         if (waterAlert != null)
             waterAlert.SetActive(false);
 
-
         drownVolumeScript = GetComponent<PlayerDrownVolume>();
     }
 
     private void Update()
     {
+        _isDeadAnim = _animator.GetBool(_isDeadHash);
         if (transform.position.y < respawnYThreshold)
         {
-
             timeInWater += Time.deltaTime;
-
 
             if (waterAlert != null)
                 waterAlert.SetActive(true);
 
             if (waterCountdown != null)
             {
-                countdownText.text = "Time Before Respawn: " + (timeThreshold - timeInWater).ToString("F0");
+                if (timeInWater <= timeThreshold)
+                {
+                    countdownText.text =
+                        "Time Before Respawn: " + (timeThreshold - timeInWater).ToString("F0");
+                }
+                else
+                {
+                    countdownText.text = "You died...";
+                }
             }
 
-
-            if (timeInWater >= timeThreshold && !isDyingTriggered)
+            if (timeInWater >= timeThreshold)
             {
-                isDyingTriggered = true;
-                Respawn();
+                KillThePlayer();
             }
 
             if (drownVolumeScript != null)
             {
                 drownVolumeScript.drowned = true;
             }
-
-
         }
-
+        // Not underwater
         else
         {
             timeInWater = 0f;
-            isDyingTriggered = false;
+            dyingAnimCountdown = 0f;
+            _animator.SetBool(_isDeadHash, false);
 
             if (waterAlert != null)
                 waterAlert.SetActive(false);
 
             if (drownVolumeScript != null)
             {
-                drownVolumeScript.drowned = false; 
+                drownVolumeScript.drowned = false;
             }
         }
-    
+    }
+
+    private void KillThePlayer()
+    {
+        _animator.SetBool(_isDeadHash, true);
+        dyingAnimCountdown += Time.deltaTime;
+        characterController.enabled = false;
+
+        if (dyingAnimCountdown >= 2.4f)
+        {
+            Respawn();
+        }
     }
 
     public void Respawn()
     {
-        characterController.enabled = false;
         transform.position = respawnPoint.position;
         characterController.enabled = true;
 
@@ -101,5 +122,4 @@ public class PlayerRespawn : MonoBehaviour
             drownVolumeScript.drowned = false;
         }
     }
-
 }
